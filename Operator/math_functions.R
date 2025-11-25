0d7f2a061ed7f7056028c778e4709e2d045302b2
SayedShaan <155416252+SayedShaan@users.noreply.github.com>
2025-11-25 10:49:03 +0530

    Operator
    All The Operator

---
+++ Operator/math_functions.R
@@ -0,0 +1,15 @@
+#Math Functions
+sqrt(81)
+log(10)
+floor(3.8)#down ward direction
+ceiling(3.2)#up ward direction
+round(3.1456,2)
+round(326.1456,-2)
+signif(3.14666,5)
+is.infinite(2/0)# it is used in calculation with bool-en method
+length(c(1,2,3))
+
+#Extreme Function
+max(1,2,6)
+min(8,6,7)
+range(8,6,7,10)
